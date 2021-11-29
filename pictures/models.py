73f7fea12eff60fa68a8240c6d8e15ce954596d8
Prayten <denvyth@gmail.com>
2021-11-29 17:37:56 +0900

    group1 added models

--- pictures/models.py
+++ pictures/models.py
@@ -1,7 +1,7 @@
 from django.db.models import *
 
 # Create your models here.
-
+'''
 d = (
     (1, 'jsahfdkl'),
     (2, 'skjdfhgkljhs'),
@@ -35,3 +35,32 @@ class F(Model):
 
 class G(F):
     pass
+    '''
+
+class Artist(Model):
+    a = CharField(max_length=30, help_text="Enter artist's name'")
+    first_name = CharField(max_length=100)
+    last_name = CharField(max_length=100)
+    about = CharField(max_length=1000)
+    country = ForeignKey('Country', verbose_name='Страна', db_index=True)
+    date_of_birth = DateField('Дата рождения', null=True, blank=True)
+    date_of_death = DateField('Дата смерти', null=True, blank=True)
+
+class Department(Model):
+    name = CharField(max_length=100)
+    phone = CharField(max_length=12)
+    employee = ForeignKey('Employee', verbose_name='Сотрудник', db_index=True)
+    placement = ForeignKey('Placement', verbose_name='Размещение', db_index=True)
+
+class Gallery(Model):
+    name = CharField(max_length=100)
+    pictures = CharField(max_length=1000)
+    phone = CharField(max_length=12)
+    address = CharField(max_length=100)
+
+    department = ForeignKey(Department, verbose_name='Отдел', db_index=True)
+    picture = ForeignKey('Picture', verbose_name='Картина', db_index=True)
+
+
+class Genre(Model):
+    name = CharField(max_length=100)
