5e2e59b36b69dec733d8b622d25e3baa6c4368c8
volvappavlov <volvappavlov62@gmail.com>
2021-12-20 16:40:34 +0900

    Models

--- pictures/views.py
+++ pictures/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from pictures.models import Picture
+from pictures.models import Picture, Employee
+
 
 # Create your views here.
 
@@ -13,5 +14,5 @@ def employee_list(request):
     return render(request, "employee.html", {'employees': Employee.objects.all()})
 
 
-def index2(request):
-    return render(request, "index2.html", {'pictures': Picture.objects.all()})
+def artist_list(request):
+    return render(request, "artist_list.html", {'artist': Picture.objects.all()})
