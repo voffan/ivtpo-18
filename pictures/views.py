a3a1b4e529eccfa22c68d7953dce60c8ba70b98f
icrynka <icrynka@gmail.com>
2021-12-20 17:02:27 +0900

    fdjhdjhf

--- pictures/views.py
+++ pictures/views.py
@@ -1,14 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from pictures.models import Picture
+from pictures.models import Picture, Employee
 
 # Create your views here.
 
 
 def index(request):
     return render(request, "index.html", {'pictures': Picture.objects.all()})
-
-
 
 
 def employee_list(request):
